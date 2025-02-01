1243b494153b6e8bda527ec93c80c8602890e371
axelrust1 <nahuel.rustt@gmail.com>
2025-02-01 18:13:05 -0300

    UPDATE

--- app.js
+++ app.js
@@ -90,6 +90,7 @@ document.getElementById("buscarParaEditar").addEventListener("click", async () =
         document.getElementById("editarNombre").value = data.nombre;
         document.getElementById("editarApellido").value = data.apellido;
         document.getElementById("editarFechaNacimiento").value = data.fechaNacimiento;
+        document.getElementById("editarTipoPersona").value = data.tipoPersona === 'PERSONA_FISICA' ? 'Física' : 'Jurídica';
         
         document.getElementById("datosParaEditar").style.display = "block";
         
@@ -121,7 +122,8 @@ document.getElementById("editarCliente").addEventListener("submit", async (event
     const clienteUpdate = {
         nombre: document.getElementById("editarNombre").value,
         apellido: document.getElementById("editarApellido").value,
-        fechaNacimiento: document.getElementById("editarFechaNacimiento").value
+        fechaNacimiento: document.getElementById("editarFechaNacimiento").value,
+        tipoPersona: document.getElementById("editarTipoPersona").value === "Jurídica" ? "J" : "F"
     };
 
     try {
